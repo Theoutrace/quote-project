ee70d04f24a839f99ab7f8cfc3ddcb717deae2b0
Theoutrace <prakashkumar.imw@gmail.com>
2022-10-28 20:40:34 +0530

    multi page single page app. react router

---
+++ src/components/quotes/QuoteList.js
@@ -0,0 +1,75 @@
+import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import QuoteItem from './QuoteItem';
+import classes from './QuoteList.module.css';
+//----------------------------------------------------------------------------------------------
+
+
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
+const QuoteList = (props) => {
+  const history = useHistory()
+  const location = useLocation()
+
+  //---------------------------------------------------------------------------------------------
+
+  console.log(location);
+
+  const queryParams =  new URLSearchParams(location.search)
+  console.log(queryParams.get('sort'));
+
+  const isSortingAscending = queryParams.get('sort')  === 'asc'
+
+
+  //---------------------------------------------------------------------------------------------
+
+  //sorting the prop.quotes and then using the data to be shown
+
+  const sortedQuotes = sortQuotes(props.quotes, isSortingAscending)
+
+  
+
+  const changeSortingHandler = ()=>{
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${(isSortingAscending? 'desc': 'asc')}`
+    })
+    // history.push(`${location.pathname}?sort=${(isSortingAscending? 'desc': 'asc')}`)
+  }
+
+  //---------------------------------------------------------------------------------------------
+
+
+
+
+  return (
+    <Fragment>
+    <div className={classes.sorting}>
+      <button onClick={changeSortingHandler}>Sort {isSortingAscending? 'Descending':'Ascending'}</button>
+    </div>
+      <ul className={classes.list}>
+        {sortedQuotes.map((quote) => (
+          <QuoteItem
+            key={quote.id}
+            id={quote.id}
+            author={quote.author}
+            text={quote.text}
+          />
+        ))}
+      </ul>
+    </Fragment>
+  );
+};
+
+export default QuoteList;
+
+// 286 done
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,66 @@
+import React, { Fragment, useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+
+const QuoteDetail = () => {
+  const params = useParams();
+  const match = useRouteMatch();
+
+  console.log("params ", params);
+
+  const { quoteId } = params;
+
+  console.log("route-match ", match);
+  //----------------------------------------------------------------------------------
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+
+  if(status === 'pending'){
+    return <div className="centered">
+      <LoadingSpinner/>
+    </div>
+  }
+
+  if(error){
+    return <p className="centered">{error}</p>
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found !</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
+        <div className="centred">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
+
+// till 289 done  
